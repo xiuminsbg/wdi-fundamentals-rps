0e6d97545fb874b1180918182339b689ccd33efc
xiuminsbg <xiuminsbg@gmail.com>
2016-05-15 10:23:57 +0800

    Unit 5 Homework Challenge: playTo(x)

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -94,3 +94,32 @@ function playToFive() {
     return ["GAME OVER. Player Score: "+playerWins, "Computer Score: "+computerWins];
 }
 
+function playTo(x) {
+    console.log("Let's play Rock, Paper, Scissors");
+    var playerWins = 0;
+    var computerWins = 0;
+    var countRound = 1;
+    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
+    while (playerWins<x && computerWins<x){
+        console.log("ROUND: "+countRound);
+	    var playerMove = getPlayerMove();
+	    var computerMove = getComputerMove();
+	    var winnerThisRound = getWinner(playerMove, computerMove);
+	    if(winnerThisRound==="Player Wins"){
+		    playerWins+=1;
+	    }else if(winnerThisRound==="Computer Wins"){
+		    computerWins+=1;
+	    }else if(winnerThisRound==="It's a tie!"){
+		    console.log("Tie");
+	    }else{
+		    console.log("Please only choose rock, scissors or paper.");
+	    }
+	    console.log("Player chose "+playerMove+" while computer chose "+computerMove+". "+"Round "+countRound+" Winner: "+winnerThisRound);
+	    console.log("Current Score: ");
+	    console.log("    Player: "+playerWins);
+	    console.log("    Computer: "+computerWins);
+	    countRound++;
+    }
+    return ["GAME OVER. Player Score: "+playerWins, "Computer Score: "+computerWins];
+}
+
